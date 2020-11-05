64d7d0adaec725f1db42d238d237a52005f4b128
Arthur Geisweiller <50842954+geisweiller@users.noreply.github.com>
2020-11-05 16:46:37 -0300

    header api integration

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,70 @@
+import React, { useEffect, useState } from 'react';
+import './styles.css';
+import { Coin } from '../Coin/index';
+import { api } from '../../services/api';
+
+interface HeaderTypes {
+
+};
+
+interface Price {
+  [key: string]:{ oldPrice: number, currentPrice: number}
+}
+
+const ALL_PRICES = {
+  BTC: {oldPrice:0, currentPrice:0},
+  LTC: {oldPrice:0, currentPrice:0},
+  ETH: {oldPrice:0, currentPrice:0},
+  LINK: {oldPrice:0, currentPrice:0},
+}
+
+
+export const Header: React.FC<HeaderTypes> = (props) => {
+  const [prices, setPrices] = useState<Price>(ALL_PRICES);
+
+
+  useEffect(() => {
+    const intervals = Object.keys(ALL_PRICES).map((coin) => {
+      return setInterval(() => {
+        api
+        .get(`price?fsym=${coin}&tsyms=BRL`)
+        .then(response => {
+          setPrices((prevState) => {
+            if(prevState[coin].currentPrice === response.data.BRL) {
+              return prevState;
+            }
+            return {
+              ...prevState,
+              [coin] : {
+                oldPrice: prevState[coin].currentPrice,
+                currentPrice: response.data.BRL
+              }
+            }
+          })
+        });
+      }, 5000);
+    });
+    return () => {
+      intervals.forEach(interval => clearInterval(interval));
+    }
+    
+    
+  }, [])
+
+  return ( 
+
+    <div className='Header'>
+        {
+          Object.keys(prices).map((coin) => {
+            return(
+              <Coin key={coin} coin={coin} oldPrice={prices[coin].oldPrice} currentPrice={prices[coin].currentPrice}/>
+            )
+          })
+        }
+  
+    </div>
+
+  )
+  
+  
+}
